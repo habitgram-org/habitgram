7142d36a45d6c89a26a60e453934571dd2f29729
Ihor Rud <Igorryd7@gmail.com>
2026-02-07 12:23:20 +0200

    feat(quote): more quotes from legends 🔥

--- app/Services/InspiringService.php
+++ app/Services/InspiringService.php
@@ -26,6 +26,8 @@ final class InspiringService
             ['Every action you take is a vote for the person you want to become', 'James Clear'],
             ['You\'ll never change your life until you change something you do daily. The secret of your success is found in your daily routine.', 'Sean Covey'],
             ['Motivation is what gets you started, habit is what keeps you going.', 'Jim Rohn'],
+            ['Discipline is doing what you hate to do, but do it like you love it.', 'Mike Tyson'],
+            ['The ability to apply discipline, the ability to do what needs to be done no matter how he feels inside, in my opinion, is the definition of a true professional.', 'Mike Tyson']
         ]);
     }
 }
