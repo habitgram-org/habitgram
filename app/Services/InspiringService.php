5e964f1af25419981b0083e466af139d92df0e46
Ihor Rud <Igorryd7@gmail.com>
2026-02-08 11:13:33 +0200

    feat(quote): more quotes from legends 🔥

--- app/Services/InspiringService.php
+++ app/Services/InspiringService.php
@@ -26,8 +26,12 @@ final class InspiringService
             ['Every action you take is a vote for the person you want to become', 'James Clear'],
             ['You\'ll never change your life until you change something you do daily. The secret of your success is found in your daily routine.', 'Sean Covey'],
             ['Motivation is what gets you started, habit is what keeps you going.', 'Jim Rohn'],
+            ['A few simple disciplines practiced every day starts to create success', 'Jim Rohn'],
+            ['Success is the natural consequence of consistently applying basic fundamentals.', 'Jim Rohn'],
+            ['Some things you to do every day. Eating seven apples on Saturday night instead of one a day just isn\'t going to get the job done.', 'Jim Rohn'],
             ['Discipline is doing what you hate to do, but do it like you love it.', 'Mike Tyson'],
-            ['The ability to apply discipline, the ability to do what needs to be done no matter how he feels inside, in my opinion, is the definition of a true professional.', 'Mike Tyson']
+            ['The ability to apply discipline, the ability to do what needs to be done no matter how he feels inside, in my opinion, is the definition of a true professional.', 'Mike Tyson'],
+            ['Where I was born and where and how I have lived is unimportant. It is what I have done with where I have been that should be of interest.', 'Georgia O\'Keeffe'],
         ]);
     }
 }
